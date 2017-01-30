a2f2ebf152074fb9d4d9f6b0df3e8cecdc0f5356
jackoverflow <leorapirap@gmail.com>
2017-01-30 16:51:06 +0800

    update file

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-$('.owl-carousel').owlCarousel({
-	autoplay: true,
-    loop:true,
-    pagination: false,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:1,
-            nav:true,
-            loop:false
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        loop:true,
+        pagination: false,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:true,
+                loop:false
+            }
         }
-    }
-})	
+    })	
+});
+
 
 (function(){
     
