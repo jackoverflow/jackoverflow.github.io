20419725641d87610af0dad69509a2d06ee7d617
jackoverflow <leorapirap@gmail.com>
2017-02-06 16:41:43 +0800

    additional files

--- js/main.js
+++ js/main.js
@@ -1,28 +1,41 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        autoplay: true,
-        loop:true,
-        pagination: false,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:1,
-                nav:false
-            },
-            1000:{
-                items:1,
-                nav:true,
-                loop:false
-            }
-        }
-    })	
+$(window).load(function(){
+    $('.flexslider').flexslider({
+    animation: "slide",
+    start: function(slider){
+        $('body').removeClass('loading');
+    }
+    });
 });
 
+$(document).ready(function(){
+    // $('.owl-carousel').owlCarousel({
+    //     autoplay: true,
+    //     loop:true,
+    //     pagination: false,
+    //     margin:10,
+    //     responsiveClass:true,
+    //     responsive:{
+    //         0:{
+    //             items:1,
+    //             nav:true
+    //         },
+    //         600:{
+    //             items:1,
+    //             nav:false
+    //         },
+    //         1000:{
+    //             items:1,
+    //             nav:true,
+    //             loop:false
+    //         }
+    //     }
+    // });
+
+    $("ul.slides li").each(function(a) {
+        $(this).find("div.slide-image").css("background-image" , 'url("' + $(this).find("img").attr("src") + '")');
+        $(this).find("img.bg").hide()
+    });     	
+});
 
 (function(){
     
