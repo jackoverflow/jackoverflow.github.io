7cb78a6b377b1033d03bdacd2b273ac91aff16bc
jackoverflow <leorapirap@gmail.com>
2017-03-11 21:52:54 +0800

    fix scrollto section

--- js/main.js
+++ js/main.js
@@ -14,10 +14,41 @@ $(document).ready(function(){
         $(this).find("img.bg").hide();
     });
 
-    $("nav a").click(function(e){
-        e.preventDefault();    
-        $('html,body').scrollTo(this.hash,this.hash, function(){
-            $('main h2').css('padding-top','90px');
-        });        
-    });
+    // $("nav a").click(function(e){
+    //     var url = $(this).attr('href')
+    //     var link = url.toString();
+    //     // alert(link);        
+
+    //     e.preventDefault();    
+    //     $('html,body').scrollTo(this.hash,this.hash, function(){
+    //         $('main h2').css('padding-top','90px');
+    //         if(link == "#news-and-events") {
+    //             $('main h2').css('padding-top','0px');
+    //         }
+    //     });   
+        
+    // });
+  // Add smooth scrolling to all links
+  $("nav a").on('click', function(event) {
+
+    event.preventDefault();
+    var url = $(this).attr('href')
+    var link = url.toString();
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });     
+
+
 });
